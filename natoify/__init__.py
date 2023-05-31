1cd295057e8f05b8af2696250b66634295b39f1d
DarkBiscuits <srecarde@gmail.com>
2023-05-31 00:40:35 -0700

    updated documentation

--- natoify/__init__.py
+++ natoify/__init__.py
@@ -1,6 +1,15 @@
 """
-The NATO_Code package contains modules for converting words and messages into their
-NATO phonetic code equivalents. 
+The Natoify package contains modules for converting plain text messages into their
+NATO phonetic code word equivalents. Each character of the message is converted into
+its NATO phonetic code word, ex- "A" becomes "Alpha", "B" becomes "Bravo", etc.
+Natoify also supports converting numbers and ascii symbols into their NATO phonetic 
+code word equivalents. Includes methods for encoding, decoding, encrypting, and 
+decrypting messages. A command line interface and tkinter desktop app is also 
+included for easy use. 
 """
 
-from . import natoify
+__version__ = "0.1.0"
+__author__ = "DarkBiscuit"
+
+
+from .natoify import Natoify
