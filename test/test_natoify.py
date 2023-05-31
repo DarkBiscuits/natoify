a56ac83e2886d8b06468834697fd4f09744d7e0a
DarkBiscuits <srecarde@gmail.com>
2023-05-31 01:38:34 -0700

    added encrypt/decrypt tests

--- test/test_natoify.py
+++ test/test_natoify.py
@@ -17,6 +17,7 @@ nato = Natoify()
 # def nato_output():
 nato_output = "TANGO HOTEL INDIA SIERRA  INDIA SIERRA  ALFA  TANGO ECHO SIERRA TANGO  MIKE ECHO SIERRA SIERRA ALFA GOLF ECHO STOP  ONE TWO THREE FOUR STOP  SIERRA TANGO OSCAR PAPA"
 nato_out_lower = "tango hotel india sierra  india sierra  alfa  tango echo sierra tango  mike echo sierra sierra alfa golf echo stop  one two three four stop  sierra tango oscar papa"
+nato_out_encrypt = "APAP RACSO OGNAT ARREIS  POTS RUOF EERHT OWT ENO  POTS OHCE FLOG AFLA ARREIS ARREIS OHCE EKIM  OGNAT ARREIS OHCE OGNAT  AFLA  ARREIS AIDNI  ARREIS AIDNI LETOH OGNAT"
 symb_output = "EXCLAMARK AT HASHTAG DOLLARSIGN PERCENT CARET AMPERSAND ASTERISK LEFTPAREN RIGHTPAREN UNDERSCORE PLUS EQUAL DASH APOSTROPHE QUOTMARK COLON SEMICOLON QUESTMARK SLASH POINT COMMA GREATERTHAN LESSTHAN BACKSLASH PIPE BACKTICK TILDE LEFTSQUARE RIGHTSQUARE LEFTCURLY RIGHTCURLY"
 numb_output = "ONE TWO THREE FOUR FIVE  SIX SEVEN EIGHT NINE ZERO"
 
@@ -29,14 +30,30 @@ def test_nato_encode():
     assert nato_message == nato_output
 
 def test_nato_decode():
-    """Test the nato_decode function
+    """Test the decode function
     """
     # Test message
     nato_message = nato_output
     message = nato.decode(nato_message)
     assert message == "THIS IS A TEST MESSAGE. 1234. STOP"
 
-def test_encode_empty():
+def test_nato_encrypt():
+    """Test the encrypt function
+    """
+    # Test message
+    message = "This is a test message. 1234. STOP"
+    nato_message = nato.encode(message, encrypt=True)
+    assert nato_message == nato_out_encrypt
+
+def test_nato_decrypt():
+    """Test the decrypt function
+    """
+    # Test message
+    nato_message = nato_out_encrypt
+    message = nato.decode(nato_message, decrypt=True)
+    assert message == "THIS IS A TEST MESSAGE. 1234. STOP"
+
+def test_nato_encode_empty():
     """Test the encode function with an empty message
     """
     # Test message
@@ -44,13 +61,13 @@ def test_encode_empty():
         nato.encode("")
 
 def test_nato_decode_empty():
-    """Test the nato_decode function with an empty message
+    """Test the decode function with an empty message
     """
     # Test message
     with pytest.raises(ValueError):
         nato.decode("")
 
-def test_encode_none():
+def test_nato_encode_none():
     """Test the encode function with a None message
     """
     # Test message
@@ -58,13 +75,13 @@ def test_encode_none():
         nato.encode(None)
 
 def test_nato_decode_none():
-    """Test the nato_decode function with a None message
+    """Test the decode function with a None message
     """
     # Test message
     with pytest.raises(ValueError):
         nato.decode(None)
 
-def test_encode_symbols():
+def test_nato_encode_symbols():
     """Test the encode function with symbols
     """
     # Test message
@@ -73,14 +90,14 @@ def test_encode_symbols():
     assert nato_message == symb_output
 
 def test_nato_decode_symbols():
-    """Test the nato_decode function with symbols
+    """Test the decode function with symbols
     """
     # Test message
     nato_message = symb_output
     message = nato.decode(nato_message)
     assert message == "!@#$%^&*()_+=-'\":;?/.,><\\|`~[]{}"
 
-def test_encode_numbers():
+def test_nato_encode_numbers():
     """Test the encode function with numbers
     """
     # Test message
@@ -89,14 +106,14 @@ def test_encode_numbers():
     assert nato_message == numb_output
 
 def test_nato_decode_numbers():
-    """Test the nato_decode function with numbers
+    """Test the decode function with numbers
     """
     # Test message
     nato_message = numb_output
     message = nato.decode(nato_message)
     assert message == "12345 67890"
 
-def test_encode_uppercase():
+def test_nato_encode_uppercase():
     """Test the encode function with uppercase letters
     """
     # Test message
@@ -105,14 +122,14 @@ def test_encode_uppercase():
     assert nato_message == nato_output
 
 def test_nato_decode_uppercase():
-    """Test the nato_decode function with uppercase letters
+    """Test the decode function with uppercase letters
     """
     # Test message
     nato_message = nato_output
     message = nato.decode(nato_message)
     assert message == "THIS IS A TEST MESSAGE. 1234. STOP"
 
-def test_encode_lowercase():
+def test_nato_encode_lowercase():
     """Test the encode function with lowercase letters
     """
     # Test message
@@ -121,7 +138,7 @@ def test_encode_lowercase():
     assert nato_message == nato_output
 
 def test_nato_decode_lowercase():
-    """Test the nato_decode function with lowercase letters
+    """Test the decode function with lowercase letters
     """
     # Test message
     nato_message = nato_out_lower
