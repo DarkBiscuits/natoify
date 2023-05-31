3166ff8ab6bef594ca84fd036e168419674f4f02
DarkBiscuits <srecarde@gmail.com>
2023-05-31 03:46:17 -0700

    testing vulgar mode

--- temp.py
+++ temp.py
@@ -1,23 +1,16 @@
 from natoify.natoify import Natoify
 
-with open("data/ChatGPT-4 Story Telling.txt", "r") as f:
+with open("data/message.txt", "r") as f:
     msg = f.read()
 
 nato = Natoify()
 
-# Unencrypted version
-# nato_msg = nato.encode(msg)
-# print(nato_msg[0:100])
-
-# with open("data/ChatGPT-4_Story_Telling_NATO.txt", "w") as f:
-#     f.write(nato_msg)
-
 # Encrypted version
-nato_msg = nato.encrypt(msg)
-print(nato_msg[0:100])
+nato.set_code("vulgar")
+nato_msg = nato.encode(msg)
+print(nato_msg)
 
-with open("data/ChatGPT-4_Story_NATO_ENCRYP.txt", "w") as f:
+with open("data/message_vulgar.txt", "w") as f:
     f.write(nato_msg)
 
-# eng_msg = nato.decode(nato_msg)
-# print(eng_msg)
+
