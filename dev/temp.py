9dedca8e41d000f89f216fe2b649385199edb93d
DarkBiscuits <srecarde@gmail.com>
2023-06-01 02:13:41 -0700

    testing

--- dev/temp.py
+++ dev/temp.py
@@ -1,6 +1,15 @@
-from natoify.natoify import Natoify
+import os
+import sys
 
-with open("data/message.txt", "r") as f:
+module_path = os.path.abspath(os.path.join('../natoify/'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+print(sys.path)
+print(os.getcwd())
+
+from natoify.engine import Natoify
+
+with open("../../data/message.txt", "r") as f:
     msg = f.read()
 
 nato = Natoify()
@@ -10,7 +19,8 @@ nato.set_code("vulgar")
 nato_msg = nato.encode(msg)
 print(nato_msg)
 
-with open("data/message_vulgar.txt", "w") as f:
-    f.write(nato_msg)
+# with open("data/message_vulgar.txt", "w") as f:
+#     f.write(nato_msg)
+# print("Wrote to data/message_vulgar.txt")
 
 
